ccae5272b13cec1a10c9ac41fb6ad1d085e1c121
zardat <paarthjoshi20@gmail.com>
2023-06-08 13:25:10 +0530

    frontend authentication workflow

---
+++ frontend/src/store/auth.js
@@ -0,0 +1,22 @@
+import {create, createStore, useStore} from 'zustand' ;
+import { mountStoreDevtool } from 'simple-zustand-devtools' ;
+
+const useAuthStore = useStore((set,get) => ({
+   allUserData: null,
+   loading: false,
+
+   user:() => ({
+      user_id : get().allUserData?.user_id||null,
+      username : get().allUserData?.username||null,
+   }),
+
+   setUser: (user) => set({ allUserData: user }),
+   setLoading: (loading) => set({ loading }),
+   isLoggedIn: () => get().allUserData !== null,
+}));
+
+if (import.meta.env.DEV) {
+   mountStoreDevtool('Store', useAuthStore);
+}
+
+export { useAuthStore };
